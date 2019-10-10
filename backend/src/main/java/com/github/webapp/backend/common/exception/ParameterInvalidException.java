8870274cd4d196071ea2c291acfb510521cbec59
wangweijiang <wangweijiang@sogou-inc.com>
2019-10-10 17:48:38 +0800

    增加基础代码 1。增加系统异常处理 2。增加统一返回结果 3。增加业务错误编码

---
+++ backend/src/main/java/com/github/webapp/backend/common/exception/ParameterInvalidException.java
@@ -0,0 +1,36 @@
+package com.github.webapp.backend.common.exception;
+
+import com.github.webapp.backend.common.enums.ResponseCode;
+
+/**
+ * @author wangweijiang
+ * @since 2019-10-08 20:35
+ */
+public class ParameterInvalidException extends BusinessException {
+    private static final long serialVersionUID = -6763391916038157647L;
+
+    public ParameterInvalidException() {
+        super();
+    }
+
+    public ParameterInvalidException(Object data) {
+        super();
+        super.data = data;
+    }
+
+    public ParameterInvalidException(ResponseCode responseCode) {
+        super(responseCode);
+    }
+
+    public ParameterInvalidException(ResponseCode responseCode, Object data) {
+        super(responseCode, data);
+    }
+
+    public ParameterInvalidException(String msg) {
+        super(msg);
+    }
+
+    public ParameterInvalidException(String formatMsg, Object... objects) {
+        super(formatMsg, objects);
+    }
+}
